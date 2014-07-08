acec26596b49af05a08e3ac67b8913888a86b562
Emily Lutz <emily.lutz@berkeley.edu>
2014-07-08 13:54:31 -0700

    Set up stubs for Direction class.

--- App_Code/classes/Direction.cs
+++ App_Code/classes/Direction.cs
@@ -8,6 +8,35 @@ namespace PathFinding
 {
     class Direction
     {
+        private Edge edge1, edge2;
+        
+        private double angle;
+
+        public double Angle
+        {
+            get {
+                if (angle == NaN)
+                {
+                    angle = calculateAngle(edge1, edge2);
+                }
+                return angle;
+            }
+            //set { angle = value; }
+        }
+
+        private double distance;
+
+        public double Distance
+        {
+            get {
+                if (distance == NaN)
+                {
+                    distance = calculateDistance(edge1, edge2);
+                }
+                return distance; 
+            }
+            //set { distance = value; }
+        }
         /**
          * Order of edges matters!
          * 
@@ -15,7 +44,24 @@ namespace PathFinding
          */
         public Direction(Edge edge1, Edge edge2)
         {
-
+            this.edge1 = edge1;
+            this.edge2 = edge2;
         }
+
+        public Direction(double angle, double distance)
+        {
+            this.angle = angle;
+            this.distance = distance;
+        }
+
+        private static double calculateAngle(Edge edge1, Edge edge2)
+        {
+            return 0.0;
+        }
+        private double calculateDistance(Edge edge1, Edge edge2)
+        {
+            return 0.0;
+        }
+
     }
 }
